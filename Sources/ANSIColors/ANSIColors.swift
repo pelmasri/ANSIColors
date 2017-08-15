67f4b5667824b4a3b9c49ed93361668f913dd525
Paesano El Masri <paesanoelmasri@gmail.com>
2017-08-15 21:04:23 +1000

    now getting it 24

--- Sources/ANSIColors/ANSIColors.swift
+++ Sources/ANSIColors/ANSIColors.swift
@@ -1,23 +1,32 @@
 public struct SelectANSI {
   
-  public enum ANSIColor: String {
-    case black = "\u{001B}[0;30m"
-    case red = "\u{001B}[0;31m"
-    case green = "\u{001B}[0;32m"
-    case yellow = "\u{001B}[0;33m"
-    case blue = "\u{001B}[0;34m"
-    case magenta = "\u{001B}[0;35m"
-    case cyan = "\u{001B}[0;36m"
-    case white = "\u{001B}[0;37m"
-    
+
+  public enum ANSIColor {
+	case named(ColorName)
   }
-  
-  static public func color(color: ANSIColor) -> String {
-    return color.rawValue
+
+  public extension ANSIColor {
+	enum ColorName: String {
+    		case black = "\u{001B}[0;30m"
+    		case red = "\u{001B}[0;31m"
+    		case green = "\u{001B}[0;32m"
+    		case yellow = "\u{001B}[0;33m"
+    		case blue = "\u{001B}[0;34m"
+    		case magenta = "\u{001B}[0;35m"
+    		case cyan = "\u{001B}[0;36m"
+    		case white = "\u{001B}[0;37m"
+    	}
   }
-  
+
+
+
+  public extension ANSIColor: CustomStringConvertible {
+	var description: String {
+		switch self {
+			case .named(let colorName):
+				return colorName.rawValue
+	}
+	}
+  }
+
 }
-
-  
-
-  
