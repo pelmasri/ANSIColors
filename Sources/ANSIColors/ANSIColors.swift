4cfb8443b753ff66d5addb4f35bff3a9d178f4dc
Paesano El Masri <paesanoelmasri@gmail.com>
2017-08-15 16:03:23 +1000

    now getting it 16

--- Sources/ANSIColors/ANSIColors.swift
+++ Sources/ANSIColors/ANSIColors.swift
@@ -1,29 +1,26 @@
 public struct AC {
   
-  public enum ANSIColor {
-    case black, red, green, yellow, blue, magenta, cyan, white
+  public var color: ANSIColor
+  init (color: ANSIColor) {
+    self.color = color
   }
   
-  public static func colorSelect(_ color: ANSIColor) -> String {
-    switch color {
-    case .black:
-      return "\u{001B}[0;30m"
-    case .red:
-      return "\u{001B}[0;31m"
-    case .green:
-      return "\u{001B}[0;32m"
-    case .yellow:
-      return "\u{001B}[0;33m"
-    case .blue:
-      return "\u{001B}[0;34m"
-    case .magenta:
-      return "\u{001B}[0;35m"
-    case .cyan:
-      return "\u{001B}[0;36m"
-    case .white:
-      return "\u{001B}[0;37m"
-    }
+  public enum ANSIColor: String {
+    case black = "\u{001B}[0;30m"
+    case red = "\u{001B}[0;31m"
+    case green = "\u{001B}[0;32m"
+    case yellow = "\u{001B}[0;33m"
+    case blue = "\u{001B}[0;34m"
+    case magenta = "\u{001B}[0;35m"
+    case cyan = "\u{001B}[0;36m"
+    case white = "\u{001B}[0;37m"
+    
   }
+  
+  public func fontColorSelect() -> String {
+    return color.rawValue
+  }
+  
 }
 
   
