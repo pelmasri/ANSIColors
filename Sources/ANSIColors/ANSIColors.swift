28adf6bcd5ff94bf42ebe0d8425f765be5d484d6
Paesano El Masri <paesanoelmasri@gmail.com>
2017-08-17 20:03:24 +1000

    170817 Final Enum version

--- Sources/ANSIColors/ANSIColors.swift
+++ Sources/ANSIColors/ANSIColors.swift
@@ -1,30 +1,19 @@
-public enum ANSIColor {
-	case named(ColorName)
+public enum ANSIColor: CustomStringConvertible {
+  
+    case black, red, green, yellow, blue, magenta, cyan, white
+  
+  public var description : String {
+    switch self {
+    case .black: return "\u{001B}[0;30m"
+    case .red: return "\u{001B}[0;31m"
+    case .green: return "\u{001B}[0;32m"
+    case .yellow: return "\u{001B}[0;33m"
+    case .blue: return "\u{001B}[0;34m"
+    case .magenta: return "\u{001B}[0;35m"
+    case .cyan: return "\u{001B}[0;36m"
+    case .white: return "\u{001B}[0;37m"
+    }
+  }
 }
-
-public extension ANSIColor {
-	public enum ColorName: String {
-    		case black = "\u{001B}[0;30m"
-    		case red = "\u{001B}[0;31m"
-    		case green = "\u{001B}[0;32m"
-    		case yellow = "\u{001B}[0;33m"
-    		case blue = "\u{001B}[0;34m"
-    		case magenta = "\u{001B}[0;35m"
-    		case cyan = "\u{001B}[0;36m"
-    		case white = "\u{001B}[0;37m"
-    	}
-}
-
-/*************************************************************************
-**									** 
-**	extension ANSIColor: CustomStringConvertible {			**
-**		public var description: String {			**
-**			switch self {					**
-**				case .named(let colorName):		**
-**					return colorName.rawValue	**
-**		}							**
-**		}							**
-**	}								**
-*************************************************************************/
 
 
